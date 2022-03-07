26c5efe27724d590615636ec90ca5197cfd83952
SpongeBed <cmodz7969@gmail.com>
2022-03-07 12:47:39 +0300

    added new classes

---
+++ The ARAI Project/The ARAI Project/FeelingsClass.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace The_ARAI_Project
+{
+    class FeelingsClass
+    {
+        //all feelings should be a value between 0 and 100
+        public int fear { get; set; }
+
+        public int happiness { get; set; }
+
+        public int sadness { get; set; }
+
+        public int anxiety { get; set; }
+
+        public int wonder { get; set; }
+
+        public int disgust { get; set; }
+
+        public int shame { get; set; }
+
+        public int anger { get; set; }
+
+
+
+    }
+}
---
+++ The ARAI Project/The ARAI Project/WorldClass.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace The_ARAI_Project
+{
+    class WorldClass
+    {
+
+        public string worldData { get; set; } //represents the 3D world information
+
+        public string characters { get; set; } //array(Containing the character IDs)
+
+
+    }
+}
